f3011b4246fc24230ad6389ba2d361efdd30824d
Ricardo Graziano <rgraziano@binwus.com>
2021-06-05 09:50:58 -0400

    init

--- src/app/appcomponent/app.component.ts
+++ src/app/appcomponent/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  users = ['Sasha', 'Connie', 'Hange', 'Mikasa', 'Eren'];
+  users: string[];
+
+  constructor(){
+    this.users = ['Minato Namikaze', 'Naruto Uzumaki', 'Nagato Uzumaki']
+  }
+
+  addUser(newUser){
+    this.users.push(newUser.value)
+    newUser.value = '';
+    newUser.focus()
+    return false; //Cancela el reseteo de la pag
+  }
+
+  deleteUser(user){
+    for(let i =0; i < this.users.length; i++){
+      if(user == this.users[i]){
+        this.users.splice(i, 1)
+      }
+    }
+  }
 }
